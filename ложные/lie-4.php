df7262a629e0276d85166ded98504a8bf391bb33
Гаджи <93768783+Gadzhii@users.noreply.github.com>
2024-06-29 19:13:37 +0300

    Delete

--- ложные/lie-4.php
+++
@@ -1,9 +0,0 @@
-<?php
-function safe_echo($data) {
-    // Эта строка безопасна, так как используется функция htmlspecialchars для экранирования вывода
-    echo htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
-}
-
-$user_input = "<script>alert('XSS');</script>";
-safe_echo($user_input);
-?>
--- ложные/lie-7.js
+++
@@ -1,8 +0,0 @@
-function safeDisplay(userInput) {
-    // Эта строка безопасна, так как используется экранирование
-    const safeContent = document.createTextNode(userInput);
-    document.body.appendChild(safeContent);
-}
-
-const input = "<script>alert('XSS');</script>";
-safeDisplay(input);
